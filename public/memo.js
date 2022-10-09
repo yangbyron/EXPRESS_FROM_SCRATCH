354b7782e34431cfbc90b1b2c13bf7df0731d5ae
root <root@LAPTOP-VI6GSBSM.localdomain>
2022-10-09 11:48:56 -0500

    change request url to api url

--- public/memo.js
+++ public/memo.js
@@ -1,4 +1,4 @@
-$.get('https://expressui.onrender.com/api/memo',(descriptions)=>{
+$.get('https://expressapi-81b8.onrender.com/api/memo',(descriptions)=>{
     let $displayDiv = $('<div class="display"></div>');
     for (let i=0;i<descriptions.length;i++){
             let $description = $(`<p class="description">${descriptions[i].description}</p><button id="${descriptions[i].memo_id}" class="delete">Delete</button>`);
@@ -9,7 +9,7 @@ $.get('https://expressui.onrender.com/api/memo',(descriptions)=>{
     $buttons = $('.delete');
     $buttons.on('click',(e)=>{
         $.ajax({
-            url: 'https://expressui.onrender.com/api/memo',
+            url: 'https://expressapi-81b8.onrender.com/api/memo',
             type: 'DELETE',
             data: {"id":e.target.id},
             success: function(){
